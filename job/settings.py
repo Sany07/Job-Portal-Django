78f632fdfe01fbfdaa6dcedfdcf8e9ff5f28a7e1
Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2025-11-04 02:21:06 +0600

    Update settings.py

--- job/settings.py
+++ job/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
 import os
 import django_heroku
 
