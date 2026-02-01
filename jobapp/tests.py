19bdadd35c956cc74a0346d3fb69f6271125efbe
yogeshjadhav3225-creator <yogeshjadhav3225@gmail.com>
2026-02-01 18:04:04 +0000

    test: add smoke test for homepage status

--- jobapp/tests.py
+++ jobapp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SmokeTests(TestCase):
+    def test_home_status_code(self):
+        """The homepage should return HTTP 200."""
+        response = self.client.get(reverse('jobapp:home'))
+        self.assertEqual(response.status_code, 200)
