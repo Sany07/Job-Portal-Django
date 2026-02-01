1a867d6a9f0adf6abe65c0709d7a026b3b04065c
yogeshjadhav3225-creator <yogeshjadhav3225@gmail.com>
2026-02-01 18:06:06 +0000

    test: avoid manifest staticfiles errors in tests by overriding STATICFILES_STORAGE

--- jobapp/tests.py
+++ jobapp/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 
 
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
 class SmokeTests(TestCase):
     def test_home_status_code(self):
         """The homepage should return HTTP 200."""
